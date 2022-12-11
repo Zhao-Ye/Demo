7131770ede18a803476eac86fdba98a7f59b1efd
Z <zhao_ye@outlook.com>
2022-12-11 17:25:24 +0800

    静态字段和静态属性性能比较

---
+++ Benchmarks/Static Field vs Static Property/ConsoleApp1/Benchmarks.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ConsoleApp1;
+
+public class Benchmarks
+{
+	[Benchmark]
+	public void StaticFieldWriteLine() => Console.WriteLine($"{StaticField.Test}");
+
+	[Benchmark]
+    public string GetStaticField() => StaticField.Test.ToString();
+
+    [Benchmark]
+	public void StaticPropertyWriteLine() => Console.WriteLine($"{StaticProperty.Test}");
+
+    [Benchmark]
+    public string GetStaticProperty() => StaticProperty.Test.ToString();
+}
+
+public class StaticField
+{
+	private readonly string _value;
+
+	private StaticField(string value)
+	{
+		this._value = value;
+	}
+
+	public override string ToString() => _value;
+
+	public static readonly StaticField Test = new StaticField("Test");
+}
+
+public class StaticProperty
+{
+	private readonly string _value;
+
+	private StaticProperty(string value)
+	{
+		this._value = value;
+	}
+
+	public override string ToString() => _value;
+
+	public static StaticProperty Test { get; } = new StaticProperty("Test");
+}
